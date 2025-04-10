2e55da6d39a3b3908842090571fa70ecb18f1eb5
abc <jc103820@ohio.edu>
2025-04-10 17:23:30 -0400

    maybe unscrewed

--- headers/transport.h
+++ headers/transport.h
@@ -1,85 +1,84 @@
 struct tcp_header {
-	u_int16_t source_port;
-	u_int16_t destination_port;
-	u_int32_t seq;
-	u_int32_t ack;
-	u_int8_t  data_offset;  // 4 bits
-	u_int8_t  flags;
-	u_int16_t window_size;
-	u_int16_t checksum;
-	u_int16_t urgent_p;
+    u_int16_t source_port;
+    u_int16_t destination_port;
+    u_int32_t seq;
+    u_int32_t ack;
+    u_int8_t data_offset;  // 4 bits
+    u_int8_t flags;
+    u_int16_t window_size;
+    u_int16_t checksum;
+    u_int16_t urgent_p;
 };
 
-void parse_tcp(int fd, tcp_header &header){
-	ssize_t bytes_read = read(fd, &header, sizeof(tcp_header));
-    
-	if (bytes_read != sizeof(tcp_header)) {
+void parse_tcp(int fd, tcp_header &header) {
+    ssize_t bytes_read = read(fd, &header, sizeof(tcp_header));
+
+    if (bytes_read != sizeof(tcp_header)) {
         printf("truncated tcp header: only %zd bytes\n", bytes_read);
         exit(1);
     }
 
-	header.source_port = ntohs(header.source_port);
-	header.destination_port = ntohs(header.destination_port);
-	header.seq = ntohl(header.seq);
-	header.ack = ntohl(header.ack);
-	header.window_size = ntohs(header.window_size);
-	header.checksum = ntohs(header.checksum);
-	header.urgent_p = ntohs(header.urgent_p);
+    header.source_port = ntohs(header.source_port);
+    header.destination_port = ntohs(header.destination_port);
+    header.seq = ntohl(header.seq);
+    header.ack = ntohl(header.ack);
+    header.window_size = ntohs(header.window_size);
+    header.checksum = ntohs(header.checksum);
+    header.urgent_p = ntohs(header.urgent_p);
 }
 
-void print_tcp_flags(u_int8_t flags){
-	printf("%c", (flags >> 0) & 1 ? 'F' : '-');
-	printf("%c", (flags >> 1) & 1 ? 'S' : '-');
-	printf("%c", (flags >> 2) & 1 ? 'R' : '-');
-	printf("%c", (flags >> 3) & 1 ? 'P' : '-');
-	printf("%c", (flags >> 4) & 1 ? 'A' : '-');
-	printf("%c\n", (flags >> 5) & 1 ? 'U' : '-');
+void print_tcp_flags(u_int8_t flags) {
+    printf("%c", (flags >> 0) & 1 ? 'F' : '-');
+    printf("%c", (flags >> 1) & 1 ? 'S' : '-');
+    printf("%c", (flags >> 2) & 1 ? 'R' : '-');
+    printf("%c", (flags >> 3) & 1 ? 'P' : '-');
+    printf("%c", (flags >> 4) & 1 ? 'A' : '-');
+    printf("%c\n", (flags >> 5) & 1 ? 'U' : '-');
 }
 
-void print_tcp(tcp_header *header){
-	printf("\tTCP:\tSport:\t%u\n", header->source_port);
-	printf("\t\tDport:\t%u\n", header->destination_port);
-	printf("\t\tFlags:\t");
-	print_tcp_flags(header->flags);
-	printf("\t\tSeq:\t%u\n", header->seq);
-	printf("\t\tACK:\t%u\n", header->ack);
-	printf("\t\tWin:\t%u\n", header->window_size);
-	printf("\t\tCSum:\t%u\n", header->checksum);
+void print_tcp(tcp_header *header) {
+    printf("\tTCP:\tSport:\t%u\n", header->source_port);
+    printf("\t\tDport:\t%u\n", header->destination_port);
+    printf("\t\tFlags:\t");
+    print_tcp_flags(header->flags);
+    printf("\t\tSeq:\t%u\n", header->seq);
+    printf("\t\tACK:\t%u\n", header->ack);
+    printf("\t\tWin:\t%u\n", header->window_size);
+    printf("\t\tCSum:\t%u\n", header->checksum);
 }
 
 struct udp_header {
-	u_int16_t source_port;
-	u_int16_t destination_port;
-	u_int16_t length;
-	u_int16_t checksum;
+    u_int16_t source_port;
+    u_int16_t destination_port;
+    u_int16_t length;
+    u_int16_t checksum;
 };
 
-void parse_udp(int fd, udp_header &header){
-	ssize_t bytes_read = read(fd, &header, sizeof(udp_header));
-    
-	if (bytes_read != sizeof(udp_header)) {
+void parse_udp(int fd, udp_header &header) {
+    ssize_t bytes_read = read(fd, &header, sizeof(udp_header));
+
+    if (bytes_read != sizeof(udp_header)) {
         printf("truncated udp header: only %zd bytes\n", bytes_read);
         exit(1);
     }
 
-	header.source_port = ntohs(header.source_port);
-	header.destination_port = ntohs(header.destination_port);
-	header.length = ntohs(header.length);
-	header.checksum = ntohs(header.checksum);
+    header.source_port = ntohs(header.source_port);
+    header.destination_port = ntohs(header.destination_port);
+    header.length = ntohs(header.length);
+    header.checksum = ntohs(header.checksum);
 }
 
-void print_udp(udp_header *header){
-	printf("\tUDP:\tSport:\t%u\n", header->source_port);
-	printf("\t\tDport:\t%u\n", header->destination_port);
-	printf("\t\tDGlen:\t%u\n", header->length);
-	printf("\t\tCSum:\t%u\n", header->checksum);
+void print_udp(udp_header *header) {
+    printf("\tUDP:\tSport:\t%u\n", header->source_port);
+    printf("\t\tDport:\t%u\n", header->destination_port);
+    printf("\t\tDGlen:\t%u\n", header->length);
+    printf("\t\tCSum:\t%u\n", header->checksum);
 }
 
-
-void compute_udp_checksum(ipv4_header &ipv4, udp_header& udp, const void* payload, uint16_t payload_len) {
+void compute_udp_checksum(ipv4_header &ipv4, udp_header &udp, const void *payload, uint16_t payload_len) {
     udp.checksum = 0;
     uint32_t sum = 0;
-    
+
     // Add pseudo-header  - for IPv4
     sum += (ipv4.source_address >> 16) & 0xFFFF;
     sum += (ipv4.source_address) & 0xFFFF;
@@ -87,67 +86,64 @@ void compute_udp_checksum(ipv4_header &ipv4, udp_header& udp, const void* payloa
     sum += (ipv4.destination_address) & 0xFFFF;
     sum += htons(IPPROTO_UDP);
     sum += htons(sizeof(udp_header) + payload_len);
-    
+
     // Add UDP header
-    uint16_t* h = (uint16_t*)&udp;
-    for (size_t i = 0; i < sizeof(udp_header)/2; i++) {
+    uint16_t *h = (uint16_t *)&udp;
+    for (size_t i = 0; i < sizeof(udp_header) / 2; i++) {
         sum += h[i];
     }
-    
+
     // payload
-    const uint16_t* p = (const uint16_t*)payload;
+    const uint16_t *p = (const uint16_t *)payload;
     size_t len = payload_len;
     while (len > 1) {
         sum += *p++;
         len -= 2;
     }
-    if (len) sum += *(uint8_t*)p; // add leftovers
-    
+    if (len) sum += *(uint8_t *)p;  // add leftovers
+
     // carry bits
     while (sum >> 16) {
         sum = (sum & 0xFFFF) + (sum >> 16);
     }
-    
-    udp.checksum = ~sum; // 1s
+
+    udp.checksum = ~sum;  // 1s
     if (udp.checksum == 0) udp.checksum = 0xFFFF;
 }
 
+void udp_ping(int fd, pcap_packet_header &pph, ethernet_header &eh, ipv4_header &ip4h, udp_header &uh, vector<unsigned char> &payload) {
+    swap(ip4h.source_address, ip4h.destination_address);
+    swap(uh.source_port, uh.destination_port);
 
-void udp_ping(int fd, pcap_packet_header &pph, ethernet_header &eh, ipv4_header &ip4h, udp_header &uh, vector<unsigned char>& payload) {
-	swap(ip4h.source_address, ip4h.destination_address);
-	swap(uh.source_port, uh.destination_port);
+    // hokey pokey -> turn yourself around
+    ip4h.total_length = ntohs(ip4h.total_length);
+    ip4h.identification = ntohs(ip4h.identification);
+    ip4h.fragmentation_offset = ntohs(ip4h.fragmentation_offset);
+    ip4h.checksum = ntohs(ip4h.checksum);
+    ip4h.source_address = ntohl(ip4h.source_address);
+    ip4h.destination_address = ntohl(ip4h.destination_address);
 
+    uh.source_port = ntohs(uh.source_port);
+    uh.destination_port = ntohs(uh.destination_port);
+    uh.length = ntohs(uh.length);
+    uh.checksum = 0;
 
-	// hokey pokey -> turn yourself around
-	ip4h.total_length = ntohs(ip4h.total_length);
-	ip4h.identification = ntohs(ip4h.identification);
-	ip4h.fragmentation_offset = ntohs(ip4h.fragmentation_offset);
-	ip4h.checksum = ntohs(ip4h.checksum);
-	ip4h.source_address = ntohl(ip4h.source_address);
-	ip4h.destination_address = ntohl(ip4h.destination_address);
+    // recompute checksum
+    compute_ipv4_checksum(ip4h);
+    uh.checksum = 0;
+    compute_udp_checksum(ip4h, uh, payload.data(), payload.size());
 
-	uh.source_port = ntohs(uh.source_port);
-	uh.destination_port = ntohs(uh.destination_port);
-	uh.length = ntohs(uh.length);
-	uh.checksum = 0;
-
-	// recompute checksum
-	compute_ipv4_checksum(ip4h);
-	uh.checksum = 0;
-	compute_udp_checksum(ip4h, uh, payload.data(), payload.size());
-
-	// send it out!
-	struct iovec iov[5] = {
+    // send it out!
+    struct iovec iov[5] = {
         {&pph, sizeof(pcap_packet_header)},
         {&eh, sizeof(ethernet_header)},
         {&ip4h, sizeof(ipv4_header)},
         {&uh, sizeof(udp_header)},
-        {payload.data(), payload.size()}
-    };
+        {payload.data(), payload.size()}};
 
-	int rval = writev(fd , iov , 5);
-	if (rval == -1){
-		perror("Error writing echo reply");
+    int rval = writev(fd, iov, 5);
+    if (rval == -1) {
+        perror("Error writing echo reply");
         exit(1);
     }
 }
@@ -155,43 +151,42 @@ void udp_ping(int fd, pcap_packet_header &pph, ethernet_header &eh, ipv4_header 
 void send_time(int fd, pcap_packet_header &pph, ethernet_header &eh, ipv4_header &ip4h, udp_header &uh) {
     time_t now;
     time(&now);
-    
+
     uint32_t time_protocol_time = htonl((uint32_t)(now + 2208988800));
 
-	ip4h.total_length = ntohs(ip4h.total_length);
-	ip4h.identification = ntohs(ip4h.identification);
-	ip4h.fragmentation_offset = ntohs(ip4h.fragmentation_offset);
-	ip4h.checksum = ntohs(ip4h.checksum);
-	ip4h.source_address = ntohl(ip4h.source_address);
-	ip4h.destination_address = ntohl(ip4h.destination_address);
+    ip4h.total_length = ntohs(ip4h.total_length);
+    ip4h.identification = ntohs(ip4h.identification);
+    ip4h.fragmentation_offset = ntohs(ip4h.fragmentation_offset);
+    ip4h.checksum = ntohs(ip4h.checksum);
+    ip4h.source_address = ntohl(ip4h.source_address);
+    ip4h.destination_address = ntohl(ip4h.destination_address);
 
-	uh.source_port = ntohs(uh.source_port);
-	uh.destination_port = ntohs(uh.destination_port);
-    
+    uh.source_port = ntohs(uh.source_port);
+    uh.destination_port = ntohs(uh.destination_port);
+
     swap(ip4h.source_address, ip4h.destination_address);
     swap(uh.source_port, uh.destination_port);
-    
+
     // update lengths
     pph.captured_length = sizeof(ethernet_header) + sizeof(ipv4_header) + sizeof(udp_header) + sizeof(time_protocol_time);
     pph.length = pph.captured_length;
     ip4h.total_length = htons(sizeof(ipv4_header) + sizeof(udp_header) + sizeof(time_protocol_time));
 
     compute_ipv4_checksum(ip4h);
-    
+
     uh.length = htons(sizeof(udp_header) + sizeof(time_protocol_time));
 
-	uh.checksum = 0;
-	compute_udp_checksum(ip4h, uh, &time_protocol_time, sizeof(time_protocol_time));
-    
+    uh.checksum = 0;
+    compute_udp_checksum(ip4h, uh, &time_protocol_time, sizeof(time_protocol_time));
+
     // send out
     struct iovec iov[5] = {
         {&pph, sizeof(pcap_packet_header)},
         {&eh, sizeof(ethernet_header)},
         {&ip4h, sizeof(ipv4_header)},
         {&uh, sizeof(udp_header)},
-        {&time_protocol_time, sizeof(time_protocol_time)}
-    };
-    
+        {&time_protocol_time, sizeof(time_protocol_time)}};
+
     int rval = writev(fd, iov, 5);
     if (rval == -1) {
         perror("Error writing TIME reply");
--- twig.cc
+++ twig.cc
@@ -1,141 +1,155 @@
-#include <stdlib.h>
-#include <iostream>
 #include <fcntl.h>
-#include <unistd.h>
 #include <netinet/in.h>
+#include <stdlib.h>
+#include <sys/uio.h>  //writev
+#include <unistd.h>
+
 #include <cstring>
 #include <fstream>
-#include <vector>
 #include <iomanip>
-#include <sys/uio.h> //writev
-#include <map> // map
-#include "headers/utils.h"
-#include "headers/pcap.h"
+#include <iostream>
+#include <map>  // map
+#include <vector>
+
 #include "headers/data_link.h"
 #include "headers/network.h"
+#include "headers/pcap.h"
 #include "headers/transport.h"
+#include "headers/utils.h"
 using namespace std;
 
 int debug = 0;
 bool correct_endian = false;
 
-bool usage(const char* prog, bool is_error) {
+bool usage(const char *prog, bool is_error) {
     printf("Usage: %s [-h] [-d|-dd|-ddd] -i <file>\n", prog);
-	return is_error;
+    return is_error;
 }
 
-int main(int argc, char *argv[]){
-	struct pcap_file_header file_header;
-	char *filename = NULL;
-	map<string,string> arp_cache;
+void process_packet(int in_fd, int out_fd, pcap_packet_header &packet_header) {
+    ethernet_header ethernet_header;
+    vector<unsigned char> payload;
 
-	for (int i = 1; i < argc; i++) {
-        if		(!strcmp(argv[i], "-i") && i+1 < argc) filename = create_network_filename(argv[++i]);
-        else if (!strcmp(argv[i], "-h"))	return usage(argv[0], 0);
-        else if (!strcmp(argv[i], "-d"))	debug = 1;
-        else if (!strcmp(argv[i], "-dd"))	debug = 2;
-        else if (!strcmp(argv[i], "-ddd"))	debug = 3;
-        else 	return usage(argv[0], 1); 
+    parse_ethernet_header(in_fd, ethernet_header);
+    print_ethernet(&ethernet_header);
+
+    int header_size = sizeof(ethernet_header);
+
+    ipv4_header ipv4_header;
+    tcp_header tcp_header;
+    udp_header udp_header;
+    icmp_header icmp_header;
+    arp_header arp_header;
+
+    if (ethernet_header.type == ETHERTYPE_IPV4) {
+        parse_ipv4(in_fd, ipv4_header);
+        print_ipv4(&ipv4_header);
+        header_size += ((ipv4_header.version_header_length & 0x0F) * 4);
+        if (ipv4_header.protocol == IPPROTOCOL_TCP) {
+            parse_tcp(in_fd, tcp_header);
+            print_tcp(&tcp_header);
+            header_size += sizeof(tcp_header);
+        } else if (ipv4_header.protocol == IPPROTOCOL_UDP) {
+            parse_udp(in_fd, udp_header);
+            print_udp(&udp_header);
+            header_size += sizeof(udp_header);
+        } else if (ipv4_header.protocol == IPPROTOCOL_ICMP) {
+            parse_icmp(in_fd, icmp_header);
+            print_icmp(&icmp_header);
+            header_size += sizeof(icmp_header);
+        }
+    } else if (ethernet_header.type == ETHERTYPE_ARP) {
+        parse_arp(in_fd, arp_header);
+        print_arp(&arp_header);
+        header_size += sizeof(arp_header);
     }
 
-	if (!filename) return usage(argv[0], 1);
+    parse_packet_content(in_fd, payload, packet_header.captured_length, header_size);
 
-	int in_fd = open(filename, O_RDONLY);
-	if (in_fd == -1) {
-		perror("Error opening input file");
-		return 1;
-	}
+    if (ethernet_header.type == ETHERTYPE_IPV4) {
+        // just saw IPV4
+        if (ipv4_header.protocol == IPPROTOCOL_TCP) {
+            // just saw TCP
+        } else if (ipv4_header.protocol == IPPROTOCOL_UDP) {
+            // just saw UDP
+            if (udp_header.destination_port == 7) {
+                printf("ECHO Echo echo echo...\n");
+                udp_ping(out_fd, packet_header, ethernet_header, ipv4_header, udp_header, payload);
+            } else if (udp_header.destination_port == 37) {
+                send_time(out_fd, packet_header, ethernet_header, ipv4_header, udp_header);
+            }
+        } else if (ipv4_header.protocol == IPPROTOCOL_ICMP && ipv4_header.destination_address == 2887745538) {  // 172.31.128.2
+            icmp_echo_reply(out_fd, packet_header, ethernet_header, ipv4_header, icmp_header, payload);
+        }
+    } else if (ethernet_header.type == ETHERTYPE_ARP) {
+        // just saw ARP
+    }
+}
 
-	int out_fd = open(filename, O_RDWR | O_CREAT | O_APPEND, 0644);
-	if (out_fd == -1) {
-		perror("Error opening output file");
-		return 1;
-	}
+int main(int argc, char *argv[]) {
+    struct pcap_file_header file_header;
+    char *filename = NULL;
+    map<string, string> arp_cache;
 
-	correct_endian = parse_file_header(in_fd, file_header);
-	print_file_header(&file_header);
+    for (int i = 1; i < argc; i++) {
+        if (!strcmp(argv[i], "-i") && i + 1 < argc)
+            filename = create_network_filename(argv[++i]);
+        else if (!strcmp(argv[i], "-h"))
+            return usage(argv[0], 0);
+        else if (!strcmp(argv[i], "-d"))
+            debug = 1;
+        else if (!strcmp(argv[i], "-dd"))
+            debug = 2;
+        else if (!strcmp(argv[i], "-ddd"))
+            debug = 3;
+        else
+            return usage(argv[0], 1);
+    }
 
-	int unsucessful_parse_attempts = 0;
+    if (!filename) return usage(argv[0], 1);
 
-		while (1){
-			pcap_packet_header packet_header;
-			ethernet_header ethernet_header;
-			vector<unsigned char> payload;
+    int in_fd = open(filename, O_RDONLY);
+    if (in_fd == -1) {
+        perror("Error opening input file");
+        return 1;
+    }
 
-			off_t current_pos = lseek(in_fd, 0, SEEK_CUR);
-			if (current_pos == -1) {
-				perror("Error getting current file position");
-				sleep(2);
-				continue;
-			}
+    int out_fd = open(filename, O_RDWR | O_CREAT | O_APPEND, 0644);
+    if (out_fd == -1) {
+        perror("Error opening output file");
+        return 1;
+    }
 
-			ssize_t bytes_read = parse_packet_header(in_fd, packet_header, correct_endian);
-	
-			if (bytes_read == 0) {
-				unsucessful_parse_attempts++;
-				if (unsucessful_parse_attempts == 5){
-					printf("listening...\n");
-				}
-				lseek(in_fd, current_pos, SEEK_SET);
-				usleep(10000);
-				continue;
-			}
+    correct_endian = parse_file_header(in_fd, file_header);
+    print_file_header(&file_header);
 
-			unsucessful_parse_attempts = 0;
+    int unsucessful_parse_attempts = 0;
 
-			parse_ethernet_header(in_fd, ethernet_header);
-			print_ethernet(&packet_header, &ethernet_header);
+    while (1) {
+        pcap_packet_header packet_header;
 
-			int header_size = sizeof(ethernet_header);
+        off_t current_pos = lseek(in_fd, 0, SEEK_CUR);
+        if (current_pos == -1) {
+            perror("Error getting current file position");
+            sleep(2);
+            continue;
+        }
 
-			ipv4_header ipv4_header;
-			tcp_header tcp_header;
-			udp_header udp_header;
-			icmp_header icmp_header;
-			arp_header arp_header;
+        ssize_t bytes_read = parse_packet_header(in_fd, packet_header, correct_endian);
 
-			if (ethernet_header.type == ETHERTYPE_IPV4){
-				parse_ipv4(in_fd, ipv4_header);
-				print_ipv4(&ipv4_header);
-				header_size += ((ipv4_header.version_header_length & 0x0F) * 4);
-				if (ipv4_header.protocol == IPPROTOCOL_TCP){
-					parse_tcp(in_fd, tcp_header);
-					print_tcp(&tcp_header);
-					header_size += sizeof(tcp_header);
-				} else if (ipv4_header.protocol == IPPROTOCOL_UDP){
-					parse_udp(in_fd, udp_header);
-					print_udp(&udp_header);
-					header_size += sizeof(udp_header);
-				} else if (ipv4_header.protocol == IPPROTOCOL_ICMP){
-					parse_icmp(in_fd, icmp_header);
-					print_icmp(&icmp_header);
-					header_size += sizeof(icmp_header);
-				}
-			} else if (ethernet_header.type == ETHERTYPE_ARP){
-				parse_arp(in_fd, arp_header);
-				print_arp(&arp_header);
-				header_size += sizeof(arp_header);
-			}
+        if (bytes_read == 0) {
+            unsucessful_parse_attempts++;
+            if (unsucessful_parse_attempts == 5) {
+                printf("listening...\n");
+            }
+            lseek(in_fd, current_pos, SEEK_SET);
+            usleep(10000);
+            continue;
+        }
 
-			parse_packet_content(in_fd, payload, packet_header.captured_length, header_size);
+        unsucessful_parse_attempts = 0;
+        print_packet_header(&packet_header);
 
-			if (ethernet_header.type == ETHERTYPE_IPV4){
-				// just saw IPV4
-				if (ipv4_header.protocol == IPPROTOCOL_TCP){
-					// just saw TCP
-				} else if (ipv4_header.protocol == IPPROTOCOL_UDP){
-					// just saw UDP
-					if (udp_header.destination_port == 7){
-						printf("ECHO Echo echo echo...\n");
-						udp_ping(out_fd, packet_header, ethernet_header, ipv4_header, udp_header, payload);
-					} else if (udp_header.destination_port == 37){
-						send_time(out_fd, packet_header, ethernet_header, ipv4_header, udp_header);
-					}
-				} else if (ipv4_header.protocol == IPPROTOCOL_ICMP && ipv4_header.destination_address == 2887745538){ // 172.31.128.2
-					icmp_echo_reply(out_fd, packet_header, ethernet_header, ipv4_header, icmp_header, payload);
-				}
-			} else if (ethernet_header.type == ETHERTYPE_ARP){
-				// just saw ARP
-			}
-		}
+        process_packet(in_fd, out_fd, packet_header);
+    }
 }
